c843da8d131f89ee4a20959c3820a525f97e6b73
室伏 美緒 <mio.murofushi@d2c.co.jp>
2020-06-18 17:24:07 +0900

    Add：app.pyにDBを追加

--- app.py
+++ app.py
@@ -52,10 +52,43 @@ def manegiment():
 def buy_order():
     add_price=""
 
-    #if "add_price" in request.form():
     add_price = request.form.get("add_price", "")
-    
-    return render_template("Vendingmachine_buy.html")
+
+    try:
+        # データベースの情報を渡し、接続
+        cnx = mysql.connector.connect(host=host, user=username, password=passwd, database=dbname)
+
+        # クエリ実行
+        cursor = cnx.cursor()
+        query = 'SELECT drink_data.drink_name, drink_data.price, manegiment_drink_number.drink_number FROM drink_data JOIN manegiment_drink_number ON drink_data.drink_id = manegiment_drink_number.drink_id; ' #実行するクエリ
+        cursor.execute(query)
+
+        order_drink_data = []
+
+        # 実行したクエリ結果の取得
+        for (drink_name, price, drink_number) in cursor:
+            item = {"drink_name":drink_name, "price":price, "drink_number":drink_number}
+            order_drink_data.append(item)
+
+        params = {
+            "order_drink_data" : order_drink_data
+        }
+        #ローカルフォルダから画像を配列にいれる
+        #glob.glob("./templates/img/*")
+
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("ユーザ名かパスワードに問題があります。")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("データベースが存在しません。")
+        else:
+            print(err)
+    else:
+
+        # DB切断
+        cnx.close()
+
+    return render_template("Vendingmachine_buy.html", **params)
 
 @app.route("/buy_result", methods=["POST"])
 def buy_result():

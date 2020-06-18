b3feff1650f83790085bcd75bed9fd63d2f64563
室伏 美緒 <mio.murofushi@d2c.co.jp>
2020-06-18 12:09:09 +0900

    Add：購入結果画面html追加

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request
+import mysql.connector
+from mysql.connector import errorcode
+import glob
 
 # データベース接続
 host = 'localhost' # データベースのホスト名又はIPアドレス
@@ -9,8 +12,46 @@ dbname   = 'my_database'    # データベース名
 app = Flask(__name__)
 @app.route("/manegiment")
 def manegiment():
-    return render_template("Manegiment_Vendingmachine.html")
+    order_manegiment = []
+    
+    try:
+        # データベースの情報を渡し、接続
+        cnx = mysql.connector.connect(host=host, user=username, password=passwd, database=dbname)
+        
+        # クエリ実行
+        cursor = cnx.cursor()
+        query = 'SELECT drink_data.drink_name, drink_data.price, manegiment_drink_number.drink_number, drink_data.publicprivate FROM drink_data JOIN manegiment_drink_number ON drink_data.drink_id = manegiment_drink_number.drink_id' #実行するクエリ
+        cursor.execute(query)
+
+        # 実行したクエリ結果の取得
+        for (drink_name, price, drink_number, publicprivate) in cursor:
+            item = {"drink_name":drink_name, "price":price, "drink_number":drink_number, "publicprivate":publicprivate}
+            order_manegiment.append(item)
+        
+        params = {
+            "order_manegiment" : order_manegiment
+        }
+        #ローカルフォルダから画像を配列にいれる
+        #glob.glob("./templates/img/*")
+
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("ユーザ名かパスワードに問題があります。")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("データベースが存在しません。")
+        else:
+            print(err)
+    else:
+
+        # DB切断
+        cnx.close()
+    
+    return render_template("Manegiment_Vendingmachine.html", **params)
 
 @app.route("/")
 def buy_order():
+    add_price=""
+
+    add_price = request.form.get("add_price", "")
+    
     return render_template("Vendingmachine_buy.html")

3ad78c9f586000a0f6371a91d021b4fbdc88fd0b
室伏 美緒 <mio.murofushi@d2c.co.jp>
2020-06-22 13:41:22 +0900

    Add：購入金額計算

--- app.py
+++ app.py
@@ -9,10 +9,19 @@ username = 'root'  # MySQLのユーザ名
 passwd   = 'Mimu1997'    # MySQLのパスワード
 dbname   = 'my_database'    # データベース名
 
+# 管理画面
 app = Flask(__name__)
 @app.route("/manegiment", methods=["GET"])
 def manegiment():
+    order_name = request.form.get("order_name", "")
+    print(order_name)
+    order_price = request.form.get("order_price", "")
+    print(order_price)
+    order_number = request.form.get("order_number", "")
+    print(order_number)
+
     order_manegiment = []
+    mes = ""
     
     try:
         # データベースの情報を渡し、接続
@@ -48,11 +57,19 @@ def manegiment():
     
     return render_template("Manegiment_Vendingmachine.html", **params)
 
-@app.route("/", methods=["GET"])
-def buy_order():
-    add_price=""
+# 購入者画面
+@app.route("/", methods=["GET","POST"])
+def buy():
+    # 初期値
+    add_price="" #投入金額
+    buy_order="" #購入商品
+    buy_drink="" #購入ボタン選択
+    mes = "" #購入確認メッセージ
 
-    add_price = request.form.get("add_price", "")
+    # 購入ボタン選択時、投入金額・購入商品情報の取得
+    if "buy_drink" in request.form.keys():
+        add_price = request.form.get("add_price")
+        buy_order = request.form.get("buy_order")
 
     try:
         # データベースの情報を渡し、接続
@@ -60,19 +77,46 @@ def buy_order():
 
         # クエリ実行
         cursor = cnx.cursor()
-        query = 'SELECT drink_data.drink_photo, drink_data.drink_name, drink_data.price, manegiment_drink_number.drink_number FROM drink_data JOIN manegiment_drink_number ON drink_data.drink_id = manegiment_drink_number.drink_id' #実行するクエリ
+        query = 'SELECT drink_data.drink_id, drink_data.drink_photo, drink_data.drink_name, drink_data.price, manegiment_drink_number.drink_number FROM drink_data JOIN manegiment_drink_number ON drink_data.drink_id = manegiment_drink_number.drink_id' #実行するクエリ
         cursor.execute(query)
 
         order_drink_data = []
+        buy = []
 
+        """
+        if "add_price" in request.form.keys():
+            if "buy_order" in request.form.keys():
+                
+        """
         # 実行したクエリ結果の取得
-        for (drink_photo, drink_name, price, drink_number) in cursor:
-            item = {"drink_photo":drink_photo, "drink_name":drink_name, "price":price, "drink_number":drink_number}
+        for (drink_id, drink_photo, drink_name, price, drink_number) in cursor:
+            item = {"drink_id":id,"drink_photo":drink_photo, "drink_name":drink_name, "price":price, "drink_number":drink_number}
             order_drink_data.append(item)
+            if item["drink_id"] == request.form.get("buy_drink"):
+                buy = item
         
+        # 購入金額計算
+        if "buy_drink" in request.form.keys():
+            buy["price"] = int(buy["price"])
+            add_price = int(add_price)
+
+            # 投入金額の確認
+            if add_price >= buy["price"]:
+                change = add_price - buy["price"]
+
+                # 在庫変更
+                stock_query = f'UPDATE drink_data SET drink_number = (buy["buy_number"]-1) WHERE drink_id = "buy["id"]"'
+                cursor.execute(stock_query)
+                cnx.commit()
+            else:
+                mes = "投入金額が足りていません。"
+
 
         params = {
-            "order_drink_data" : order_drink_data
+            "order_drink_data" : order_drink_data,
+            "add_price":add_price,
+            "buy_order":buy_order,
+            "mes" : mes
         }
         #ローカルフォルダから画像を配列にいれる
         #glob.glob("./templates/img/*")

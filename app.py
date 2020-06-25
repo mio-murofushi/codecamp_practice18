c2ae677fb3df5d586bf7b2e40fffd727c6a3fec0
室伏 美緒 <mio.murofushi@d2c.co.jp>
2020-06-25 16:30:28 +0900

    Add:追加商品の写真パス取得

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import mysql.connector
 from mysql.connector import errorcode
 import glob
 import re
+import os
+#from PIL import Image
+from werkzeug.utils import secure_filename
+
 
 # データベース接続
 host = 'localhost' # データベースのホスト名又はIPアドレス
@@ -10,28 +14,35 @@ username = 'root'  # MySQLのユーザ名
 passwd   = 'Mimu1997'    # MySQLのパスワード
 dbname   = 'my_database'    # データベース名
 
-# 管理画面
+
 app = Flask(__name__)
+
+# フォルダ
+UPLOAD_FOLDER = './static/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# 管理画面
 @app.route("/manegiment", methods=["GET","POST"])
 def manegiment():
     order_manegiment = []
     mes = ""
     add_filename = "" #追加商品のパス指定
-    #filename
+    file_name = ""
     
     # 商品追加のボタンをおした時、新商品情報を取得
-    if "add" in request.form.keys():
+    if "add" in request.form.keys() and "file_name" in request.files:
         order_name = request.form.get("order_name", "")
         order_price = request.form.get("order_price", "")
         order_number = request.form.get("order_number", "")
-        filename = request.form.get("filename", "")
+        file_name = request.files["file_name"]
         """
         # ファイル情報取得
-        if "filename" in request.files:
-            filename = request.files["filename"]
+        if "file_name" in request.files:
+            file_name = request.files["file_name"]
         else:
-            filename = ""
+            file_name = ""
         """
+
         publicprivate = request.form.get("publicprivate", "")
     
     try:
@@ -41,16 +52,18 @@ def manegiment():
         # 新商品情報を取得した場合
         if "add" in request.form.keys():
             # 入力欄に記入漏れがあるか、否か
-            if order_name=="" or order_price=="" or order_number=="" or filename=="":
+            if order_name=="" or order_price=="" or order_number=="" or file_name=="":
                 mes = "入力欄が不十分です。"
             else:
                 # 新商品の価格、在庫が数字であるか、否か
                 if str.isnumeric(order_price) == True and str.isnumeric(order_number) == True:
                     # 新商品のファイルネームがPNG,JPEGであるか、否か
-                    if re.match("([^\s]+(\.(?i)(jpeg|png))$)", filename):
+                    if re.match("([^\s]+(\.(?i)(jpeg|jpg|png))$)", file_name):
+                        add_filename.save(os.path.join(app.config['UPLOAD_FOLDER'], file_name))
+                        
                         # 新商品の情報をクエリに追加
                         cursor = cnx.cursor()
-                        new_order_query=F"INSERT INTO drink_data(drink_name, price, publicprivate, drink_photo) VALUES ('{order_name}', {order_price}, {publicprivate}, '{filename}')"
+                        new_order_query=F"INSERT INTO drink_data(drink_name, price, publicprivate, drink_photo) VALUES ('{order_name}', {order_price}, {publicprivate}, '{file_name}')"
                         new_stock_query=F"INSERT INTO manegiment_drink_number(drink_number) VALUES ({order_number})"
                         cursor.execute(new_order_query)
                         cnx.commit()
